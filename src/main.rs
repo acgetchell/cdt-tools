524c55026d939c7c6708c0487dc062c795b7e86a
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-09 18:28:38 +1000

    Fix the code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-extern crate blas;
-extern crate lapack;
-extern crate blas_src;
-
-use plotpy::Plot;
 use tritet::{StrError, Tetgen};
 
 fn main() -> Result<(), StrError> {
@@ -10,20 +5,17 @@ fn main() -> Result<(), StrError> {
     let mut tetgen = Tetgen::new(8, None, None, None)?;
 
     // set points
+    let marker = 0;
     tetgen
-        .set_point(0, 0.0, 0.0, 0.0)?
-        .set_point(1, 1.0, 0.0, 0.0)?
-        .set_point(3, 0.0, 1.0, 0.0)?
-        .set_point(4, 0.0, 0.0, 1.0)?
-        .set_point(5, 1.0, 0.0, 1.0)?
-        .set_point(6, 1.0, 1.0, 1.0)?
-        .set_point(7, 0.0, 1.0, 1.0)?;
+        .set_point(0, marker, 0.0, 0.0, 0.0)?
+        .set_point(1, marker, 1.0, 0.0, 0.0)?
+        .set_point(3, marker, 0.0, 1.0, 0.0)?
+        .set_point(4, marker, 0.0, 0.0, 1.0)?
+        .set_point(5, marker, 1.0, 0.0, 1.0)?
+        .set_point(6, marker, 1.0, 1.0, 1.0)?
+        .set_point(7, marker, 0.0, 1.0, 1.0)?;
 
     // generate Delaunay triangulation
     tetgen.generate_delaunay(false)?;
-
-    // draw edges of tetrahedra
-    let _plot = Plot::new();
-
     Ok(())
 }
